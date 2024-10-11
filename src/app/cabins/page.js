8aacdd0c020397fe16161e4ea6b4b3384e0bbf45
Oms-343 <oms87473@gmail.com>
2024-10-11 10:39:29 +0530

    adding the filtering functionality for the cabins

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,14 +1,18 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
 };
 
 export const revalidate = 3600;
+// this page is no longer staticly generated as we have used searchParams
 
-function Page() {
+function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +27,12 @@ function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
